91e59fe1035bea72d2812ca85927c56bd7fd0119
tao <taowuu@foxmail.com>
2022-11-04 19:31:10 +0800

    第一步完成

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <p>counter: {{$store.state.counter}}</p>
+    <button @click="$store.dispatch('add')"> async add </button>
+    <div></div>
+    <button @click="$store.commit('add')"> add </button>
   </div>
 </template>
 
